8e74cbb6ed2a5f7b48fc285cb623b84d07c7dd32
Daniel Kopecky <avion123678@icloud.com>
2021-02-20 12:57:27 +0300

    script21 script22

---
+++ 1st_lab/script21.cmd
@@ -0,0 +1,19 @@
+@echo off
+
+set /p STR="Enter STR: "
+set user_name=UPart2%STR%
+set group_name=GPart2%STR%
+
+echo User %user_name% creating:
+net user %user_name% /add
+
+echo Group %group_name% creating:
+net localgroup %group_name% /add
+
+echo Adding user %user_name% to group %group_name%:
+net localgroup %group_name% %user_name% /add
+
+echo Activating user %user_name%:
+net user %user_name% /active:yes
+
+pause
---
+++ 1st_lab/script22.cmd
@@ -0,0 +1,13 @@
+@echo off
+
+set /p mode="Choose the mode: "
+
+if "%mode%" == "Manual" (
+	netsh interface ipv4 set dns name="Ethernet" static 8.8.8.8
+	netsh interface ipv4 set address name="Ethernet" static 192.168.1.10 255.255.255.0 192.168.1.1 
+) else (
+	netsh interface ip set address "Ethernet" dhcp
+	netsh interface ip set dns "Ethernet" dhcp
+)
+
+pause
