07005c0568fc7a8204d9d75870f7b9beea73f452
avion123678 <avion123678@gmail.com>
2021-03-22 18:01:50 +0300

    Update base_script.ps1

--- 2nd_lab/base_script.ps1
+++ 2nd_lab/base_script.ps1
@@ -1,2 +1,2 @@
 Invoke-Command -ComputerName 10.0.0.1, 10.0.0.2 -ScriptBlock {Install-WindowsFeature -Name "DHCP" -IncludeManagementTools}
-Invoke-Command -ComputerName 10.0.0.1 -FilePath c:\dhcp_remote_script\script.ps1
+Invoke-Command -ComputerName 10.0.0.1 -FilePath c:\dhcp_remote_script\dhcp_remote_script.ps1
